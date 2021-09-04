98b4d3cc45ec32dbb4f3fb152145d012921fe832
sawand98d <sawant65d@gmail.com>
2021-09-04 21:05:34 +0530

    in textutils prject added templates

--- textutils/textutils/views.py
+++ textutils/textutils/views.py
@@ -1,10 +1,8 @@
 # I have created this views.py file :- sawant98d
 from django.http import HttpResponse
-
+from django.shortcuts import render
 
 """
-
-
 def about(request):
     return HttpResponse("<h4>About page</h4>")
 
@@ -19,16 +17,18 @@ def get_links(request):
     ''')
 """
 def index(request):
-    return HttpResponse("<h1>Hello welcome page</h1>")
+    #return HttpResponse("<a href='/'>Back</a><h1>Hello welcome page</h1>")
+    params = {'name':'Dadasaheb', 'place':'India'}
+    return render(request,'index.html',params)
 
 def removepunc(request):
-    return HttpResponse('removepunc')
+    return HttpResponse("<a href='/'>Back</a>removepunc")
 
 def capitalizefirst(request):
-    return HttpResponse('capitalizefirst')
+    return HttpResponse("<a href='/'>Back</a>capitalizefirst")
 
 def newlineremove(request):
-    return HttpResponse('newlineremove')
+    return HttpResponse("<a href='/'>Back</a>newlineremove")
 
 def spaceremove(request):
     return HttpResponse('spaceremove')
